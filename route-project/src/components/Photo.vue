0c42d21d4421b39f4758fce990a7ab106d0bc4ae
Mario Nemuel Flores Condori <marioflorescondori@gmail.com>
2019-03-06 11:48:22 -0400

    video 20 - a view has a lot of components

---
+++ route-project/src/components/Photo.vue
@@ -0,0 +1,12 @@
+<template>
+	<div>
+		<h1>Photo Component : {{$route.params.id}}</h1>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'Photo'
+	}
+
+</script>
---
+++ route-project/src/views/Photos.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <h1>Galery Photos</h1>
+    <Photo></Photo>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Photo from '@/components/Photo.vue'
+
+export default {
+  name: 'home',
+  components: {
+    Photo
+  }
+}
+</script>
